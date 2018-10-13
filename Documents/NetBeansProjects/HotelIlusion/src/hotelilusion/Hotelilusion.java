7211dfcbb65237d20b2cf0f5ce7b95c0c61d9acc
Marcela Angella <huthofe@gmail.com>
2018-10-12 23:31:14 -0300

    correcciones

---
+++ Documents/NetBeansProjects/HotelIlusion/src/hotelilusion/Hotelilusion.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotelilusion;
+
+import java.text.ParseException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import hotelilusion.modelo.Conexion;
+import hotelilusion.modelo.HuespedData;
+
+/**
+ *
+ *  * @author agus
+ */
+public class Hotelilusion {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.text.ParseException
+     */
+    public static void main(String[] args) throws ParseException {
+        // TODO code application logic here
+
+                }
+    }
+    
--- Documents/NetBeansProjects/HotelIlusion/test/Test.java
+++ Documents/NetBeansProjects/HotelIlusion/test/Test.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package hotelilusion;
+
 
 
 import hotelilusion.modelo.Huesped;
